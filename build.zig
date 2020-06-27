4b84b8eb1c630a2d7e1f12caa053835b329d24a2
Marnix Klooster <marnix.klooster@solcon.nl>
2020-06-27 11:10:47 +0200

    Read file set.mm into memory

--- build.zig
+++ build.zig
@@ -1,11 +1,16 @@
 const Builder = @import("std").build.Builder;
-const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+
     var exe = b.addExecutable("zigmmverify", "src/main.zig");
     exe.setBuildMode(mode);
+    exe.linkSystemLibrary("c");
+
     var tst = b.addTest("src/main.zig");
+
     tst.step.dependOn(&exe.step);
     b.default_step.dependOn(&tst.step);
+
+    exe.install();
 }
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,18 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.assert(false);
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    const mm_file = try std.fs.cwd().openFile("set.mm", .{});
+    defer mm_file.close();
+    const size = (try mm_file.stat()).size;
+    const buf = try allocator.alloc(u8, size);
+    defer allocator.free(buf);
+    _ = try mm_file.readAll(buf);
 }
 
-test "always succeed" {
-    std.debug.assert(true);
+test "run main" {
+    try main();
 }
