4dabb290abe2453db4303af04eccb7308f17b0c5
Marnix Klooster <marnix.klooster@solcon.nl>
2020-07-02 15:19:36 +0200

    Verify code: initial data structure ideas

--- src/verify.zig
+++ src/verify.zig
@@ -1,7 +1,25 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
+const tokenize = @import("tokenize.zig");
+const TokenList = tokenize.TokenList;
 const parse = @import("parse.zig");
+
+const Expression = std.SegmentedList(struct { token: Token, cv: enum { C, V } }, 0);
+const InferenceRule = struct {
+    hypotheses: std.SegmentedList(struct { expression: Expression, isF: bool }, 0),
+    conclusion: Expression,
+};
+const Substitution = std.AutoHashMap(Token, Expression);
+const ProofState = std.SinglyLinkedList(Expression);
+const VerifyState = struct {
+    activeFEStatements: std.SegmentedList(struct {
+        label: Token, expression: Expression, ef: enum { E, F }
+    }, 0),
+    activeStatements: std.AutoHashMap(Token, InferenceRule),
+    scopes: ScopeStack,
+};
+const ScopeStack = std.SinglyLinkedList(std.AutoHashMap(Token, void));
 
 pub fn verify(buffer: []u8, allocator: *Allocator) !void {
     var statements = parse.StatementIterator.init(allocator, buffer);
@@ -14,3 +32,5 @@ pub fn verify(buffer: []u8, allocator: *Allocator) !void {
         statement.deinit(allocator);
     }
 }
+
+test "" {}
