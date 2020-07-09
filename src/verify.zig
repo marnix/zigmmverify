965f12380d5205eda11aa37eed7f6267e142b65b
Marnix Klooster <marnix.klooster+github.com@gmail.com>
2020-07-09 21:50:36 +0200

    Verify: Fixed memory leak, added tests.

--- src/verify.zig
+++ src/verify.zig
@@ -35,7 +35,7 @@ const Meaning = union(MeaningType) {
     const Self = @This();
 
     Constant: void,
-    Variable: void,
+    Variable: ?Token,
     Rule: InferenceRule,
 
     fn deinit(self: *Self, allocator: *Allocator) void {
@@ -127,16 +127,20 @@ const ScopeDiff = struct {
 
     fn pop(self: *Self) void {
         self.state.currentScopeDiff = self.optOuter;
+        self.deinitActiveTokens();
+        self.state.allocator.destroy(self);
+    }
 
+    fn deinitActiveTokens(self: *Self) void {
         var it = self.activeTokens.iterator();
         while (it.next()) |kv| {
-            if (self.state.meanings.remove(kv.key)) |_| {} else {
+            if (self.state.meanings.remove(kv.key)) |*kv2| {
+                kv2.value.deinit(self.state.allocator);
+            } else {
                 std.debug.warn("\nTODO: insert meaning for token {0}.\n", .{kv.key});
             }
         }
         self.activeTokens.deinit();
-
-        self.state.allocator.destroy(self);
     }
 };
 
@@ -166,7 +170,7 @@ pub fn verify(buffer: []const u8, allocator: *Allocator) !void {
             .V => |vStatement| {
                 var it = @as(TokenList, vStatement.variables).iterator(0); // TODO: why coercion needed??
                 while (it.next()) |variable| {
-                    const kv = try state.meanings.put(variable.*, MeaningType.Variable);
+                    const kv = try state.meanings.put(variable.*, Meaning{ .Variable = null });
                     if (kv) |_| return Error.Duplicate;
                     if (state.currentScopeDiff) |scopeDiff| {
                         _ = try scopeDiff.activeTokens.add(variable.*); // this $v will become inactive at the next $}
@@ -175,14 +179,14 @@ pub fn verify(buffer: []const u8, allocator: *Allocator) !void {
             },
             .F => |fStatement| {
                 const kv = try state.meanings.put(fStatement.label, Meaning{ .Rule = try state.fromHypothesis(fStatement.tokens) });
-                if (kv) |_| return Error.Duplicate;
+                if (kv) |_| return Error.Duplicate; // check _before_ put
                 if (state.currentScopeDiff) |scopeDiff| {
                     _ = try scopeDiff.activeTokens.add(fStatement.label); // this $f will become inactive at the next $}
                 }
             },
             .E => |eStatement| {
                 const kv = try state.meanings.put(eStatement.label, Meaning{ .Rule = try state.fromHypothesis(eStatement.tokens) });
-                if (kv) |_| return Error.Duplicate;
+                if (kv) |_| return Error.Duplicate; // check _before_ put
                 if (state.currentScopeDiff) |scopeDiff| {
                     _ = try scopeDiff.activeTokens.add(eStatement.label); // this $e will become inactive at the next $}
                 }
@@ -213,6 +217,10 @@ test "use undeclared variable" {
     expectError(Error.UnexpectedToken, verify("$c wff $. wph $f wff ph $.", std.testing.allocator));
 }
 
+test "use undeclared variable" {
+    expectError(Error.UnexpectedToken, verify("$c wff $. wph $f wff ph $.", std.testing.allocator));
+}
+
 test "use statement label as a token" {
     expectError(Error.UnexpectedToken, verify("$c wff ph $. wph $f wff ph $. wxx $e wph $.", std.testing.allocator));
 }
@@ -226,7 +234,7 @@ test "tokenlist to expression" {
     var state = try VerifyState.init(std.testing.allocator);
     defer state.deinit();
     _ = try state.meanings.put("wff", MeaningType.Constant);
-    _ = try state.meanings.put("ph", MeaningType.Variable);
+    _ = try state.meanings.put("ph", Meaning{ .Variable = null });
     var tokens = TokenList.init(std.testing.allocator);
     defer tokens.deinit();
     try tokens.push("wff");
@@ -241,6 +249,30 @@ test "tokenlist to expression" {
     expect(expression[0].cv == .C);
     expect(eq(expression[1].token, "ph"));
     expect(expression[1].cv == .V);
+}
+
+//test "no duplicate variable declarations" {
+//    expectError(Error.Duplicate, verify("$c ca cb $. $v v $. cav $f ca v $. cbv $f cb v $.", std.testing.allocator));
+//}
+
+//test "no duplicate variable declarations, in nested scope (2)" {
+//    expectError(Error.Duplicate, verify("$c ca cb $. ${ $v v $. cav $f ca v $. cbv $f cb v $. $}", std.testing.allocator));
+//}
+
+//test "duplicate variable declarations, in nested scope (2)" {
+//    try verify("$c ca cb $. $v v $. ${ cav $f ca v $. $} cbv $f cb v $.", std.testing.allocator);
+//}
+
+test "$v in nested scope" {
+    try verify("$c ca $. ${ $v v $. $}", std.testing.allocator);
+}
+
+test "$f in nested scope (1)" {
+    try verify("$c ca $. ${ $v v $. cav $f ca v $. $}", std.testing.allocator);
+}
+
+test "$f in nested scope (2)" {
+    try verify("$c ca $. $v v $. ${ cav $f ca v $. $}", std.testing.allocator);
 }
 
 test "token is either constant or variable, not both" {
